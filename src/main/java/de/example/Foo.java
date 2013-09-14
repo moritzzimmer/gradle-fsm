ef8e263ea9959571c802b3d010d81e4f38b2c316
moritzzimmer <moritz.zmmr@gmail.com>
2013-09-14 21:05:35 +0200

    sample project including fsm build script

---
+++ src/main/java/de/example/Foo.java
@@ -0,0 +1,17 @@
+package de.example;
+
+import org.joda.time.DateTime;
+
+
+public class Foo {
+  
+  private DateTime date;
+  
+  public DateTime getDate() {
+    return date;
+  }
+  
+  public void setDate(DateTime date) {
+    this.date = date;
+  }
+}
---
+++ src/test/java/de/example/FooTest.java
@@ -0,0 +1,12 @@
+package de.example;
+
+import org.junit.Test;
+
+
+public class FooTest {
+
+  @Test
+  public void test() {
+  }
+
+}
